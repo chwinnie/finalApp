280f90aa0ab6a1bc09bbd4c7e2c46082da7cb5db
chwinnie <winnie.r.cheng@gmail.com>
2015-02-16 23:05:54 -0500

    itineraries added

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,7 +1,23 @@
 class ItinerariesController < ApplicationController
   def new
+  	@itinerary = itinerary.new
+  end
+
+  def create
+  	@itinerary = itinerary.new(itinerary_params)
+  	if @itinerary.save
+  		redirect_to itinerarys_path
+  	else
+  		render 'new'
+  	end
   end
 
   def index
+  	@itineraries=itinerary.all
+  end
+
+  private
+  def itinerary_params
+  	params.require(:itinerary).permit(:name, :location)
   end
 end
