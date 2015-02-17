64c30b863fc7eef36de821d2fdddf1379cef24d4
chwinnie <winnie.r.cheng@gmail.com>
2015-02-16 22:49:07 -0500

    found mistake in trip index html

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217015550) do
+ActiveRecord::Schema.define(version: 20150217033041) do
+
+  create_table "trips", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
